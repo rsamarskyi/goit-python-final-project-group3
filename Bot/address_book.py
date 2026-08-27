dac387a0cd5cff5fbd4212910e4f14054e619246
Rodion P <rodion.podkorytov@gmail.com>
2026-08-27 21:06:05 +0300

    remade so that NoteBook is the separate class, need to add helper functions

--- Bot/address_book.py
+++ Bot/address_book.py
@@ -52,10 +52,35 @@ class Birthday(Field):
         super().__init__(birthday_date)
 
 class Note(Field):
-    def __init__(self, note, c_id):
+    def __init__(self, title, note):
         self.value = note
-        self.c_id = c_id
+        self.title = title
 
+class NoteBook(UserDict):
+    def add_note(self, title, text):
+        note = Note(title, text)
+        self.data[title] = note
+        
+
+    def find(self,title):
+        return self.data.get(title)
+
+    def edit_note(self, title, new_text):
+        old_note = self.find(title)
+        if not old_note:
+            raise NoteError('No note with such title')
+        new_note = Note(title, new_text)
+        self.data[title] = new_note
+
+    def search_note(self, search_p):
+        notes = []
+        for note in self.data.values():
+            if search_p.lower() in note.value.lower():
+                notes.append({ "title": note.title, "text": note.value})
+        return notes
+
+    def delete_note(self, title):
+        self.data.pop(title, None)
 
 class Record:
     def __init__(self, name):
@@ -64,8 +89,6 @@ class Record:
         self.birthday = None
         self.email = None
         self.address = None
-        self.notes = {}
-        self.next_note_id = 0
 
     def __str__(self):
         return f"""Contact name: {self.name.value},
